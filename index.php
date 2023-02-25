a946c7efc03413389e702b11f02543f77200f951
marcusalexandra <marcusalexandra2@gmail.com>
2023-02-25 16:49:45 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BAG PULA</title>
+    <title>Library</title>
 </head>
 <body>
-    <h3>IMI BAG TOATA PULA</h3>
+    <h3>Test</h3>
 </body>
 </html>
